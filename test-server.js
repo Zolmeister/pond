a2c0c0e7ec03a362ebfb75bebe898d1b19ed37be
Zolmeister <zolikahan@gmail.com>
2016-09-23 10:46:53 -0500

    dockerize

--- test-server.js
+++ test-server.js
@@ -1,39 +1,24 @@
-
-/**
- * Module dependencies.
- */
-
 var express = require('express')
-  , http = require('http')
   , path = require('path');
 
 var app = express();
-
-app.configure(function(){
-  app.set('port', process.env.PORT || 3000);
-  app.use(express.logger('dev'));
-  app.use(app.router);
-  app.use(express.static(path.join(__dirname, '')));
-});
-
-app.configure('development', function(){
-  app.use(express.errorHandler());
-});
+var port = process.env.PORT || 3000;
 
 app.all('/', function(req, res){
-	res.sendfile('index.html');
+	res.sendFile(path.join(__dirname, 'index.html'));
 });
+
+app.get('/ping', function (req, res) {
+  res.send('pong');
+})
 
 app.get('/manifest.webapp', function(req, res){
 	res.header('Content-Type', 'application/x-web-app-manifest+json');
 	res.sendfile('manifest.webapp');
 });
 
-app.get('/log', function(req, res){
-  console.log(req.param('l'))
-  res.end()
-})
+app.use(express.static(path.join(__dirname, '')));
 
-http.createServer(app).listen(app.get('port'), function(){
-  console.log("Express server listening on port " + app.get('port'));
+app.listen(port, function(){
+  console.log("Express server listening on port " + port);
 });
